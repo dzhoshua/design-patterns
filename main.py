2f7895c460b5c1a0686efcb93ecf6f603bd54e24
dzhoshua <kozicinalk@gmail.com>
2024-10-12 20:12:05 +0800

    add swagger

---
+++ main.py
@@ -0,0 +1,53 @@
+import connexion
+from Src.Core.format_reporting import format_reporting
+from Src.Reports.report_factory import report_factory
+from Src.data_reposity import data_reposity
+from Src.settings_manager import settings_manager
+from Src.start_service import start_service
+
+app = connexion.FlaskApp(__name__)
+manager = settings_manager()
+manager.open("settings.json")
+reposity = data_reposity()
+start = start_service(reposity)
+start.create()
+
+@app.route("/api/reports/formats", methods=["GET"])
+def formats():
+    return [{"name":item.name, "value":item.value} for item in format_reporting]
+
+@app.route("/api/reports/range/<format>", methods=["GET"])
+def reports_range(format: str):
+    inner_format = format_reporting(format)
+    report = report_factory(manager).create(inner_format)
+    report.create( reposity.data[ data_reposity.range_key()  ] )
+
+    return report.result
+
+@app.route("/api/reports/group/<format>", methods=["GET"])
+def reports_group(format: str):
+    inner_format = format_reporting(format)
+    report = report_factory(manager).create(inner_format)
+    report.create( reposity.data[ data_reposity.group_key()  ] )
+
+    return report.result
+
+@app.route("/api/reports/nomenclature/<format>", methods=["GET"])
+def reports_nomenclature(format: str):
+    inner_format = format_reporting(format)
+    report = report_factory(manager).create(inner_format)
+    report.create( reposity.data[ data_reposity.nomenclature_key()  ] )
+
+    return report.result
+
+@app.route("/api/reports/recipe/<format>", methods=["GET"])
+def reports_recipe(format: str):
+    inner_format = format_reporting(format)
+    report = report_factory(manager).create(inner_format)
+    report.create( reposity.data[ data_reposity.receipt_key()  ] )
+
+    return report.result
+
+if __name__ == "__main__":
+    app.add_api("swagger.yaml")
+    app.run(port = 8080)
